b102426f36e674d0fa333d65cfdf11213957c59d
Kseny22 <misyura.c@yandex.ru>
2025-11-02 02:47:48 +0300

    IdlingResources

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IdilingTest.java
@@ -0,0 +1,78 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.allOf;
+
+import android.view.View;
+
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@LargeTest
+@RunWith(AllureAndroidJUnit4.class)
+public class IdilingTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+    @Before
+    public void registerIdLingResources() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+    @After
+    public void unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+
+    }
+    @Test
+    public void testOpenGallery() {
+        ViewInteraction menu = onView(isAssignableFrom(AppCompatImageButton.class));
+        menu.check(matches(isDisplayed()));
+        menu.perform(click());
+
+        ViewInteraction gallery = onView(withId(R.id.nav_gallery));
+        gallery.perform(click());
+
+ //       ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
+ //       recyclerView.check(CustomViewAssertions.isRecyclerView());
+//        recyclerView.check(matches(CustomViewMatcher.recyclerViewSizeMatcher(9)));
+
+        ViewInteraction itemSeven = onView(allOf(withId(R.id.item_number), withText("7")));
+        itemSeven.check(matches(withText("7")));
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+}
